be4609bd3079692ef86417509f8430779d2ab7cc
Gabriel Saiago <grsaiago@gmail.com>
2025-11-08 18:26:30 -0300

    add wip start on main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <unistd.h>
+
+int start_traced_process(char *cmd) {
+	extern char **environ;
+	char         *argv[] = {"/bin/ls", NULL};
+	int           pid;
+	int           err;
+
+	pid = fork();
+	if (pid == 0) {
+		err = ptrace(PTRACE_TRACEME);
+		if (err != 0) {
+			printf("failed to PTRACE_TRACME in child process\n");
+			return (EXIT_FAILURE);
+		}
+		execve(cmd, argv, environ);
+	}
+	return (EXIT_SUCCESS);
+}
 
 int main(void) {
-	printf("oieoie\n");
+	start_traced_process("/bin/ls");
+	return (EXIT_SUCCESS);
 }
